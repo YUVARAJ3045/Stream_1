fa7581cbc93b076285f2dc047a71105cf5089124
YUVARAJ3045 <104370864+YUVARAJ3045@users.noreply.github.com>
2023-05-24 12:42:02 +0530

    Adding breakfast menu

--- stream_app.py
+++ stream_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
-streamlit.title('Hi SUshmitha')
+streamlit.title('Hi Sushmitha')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text( 🐔'Hard-Boiled Free-Range Egg')
+streamlit.text( 🥑🍞'Avacoda Toast')
