bc6edf0c42d9eb6868cc6627c44c4e267802d4ac
YUVARAJ3045 <104370864+YUVARAJ3045@users.noreply.github.com>
2023-05-24 14:13:21 +0530

    Selected fruit

--- stream_app.py
+++ stream_app.py
@@ -15,7 +15,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') #seting index as fruit
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(my_fruit_list)#display all fruit list
+streamlit.dataframe(my_fruit_list)#display selected fruit list
